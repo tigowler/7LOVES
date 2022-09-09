d0105ce1a8e2b707a59e018a8559d091b8b95b1f
nneo <aisapple1030@naver.com>
2022-09-09 20:52:04 +0900

    [FEAT] complete header CSS

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,13 @@ import styles from "./header.module.css";
 import React from "react";
 
 const Header = ({ onLogout, userName }) => (
-  <header>
-    {userName && <h4>{userName}</h4>}
-    {onLogout && <button onClick={onLogout}>Logout</button>}
+  <header className={styles.header}>
+    {userName && <h4 className={styles.userName}>{userName}</h4>}
+    {onLogout && (
+      <button className={styles.button} onClick={onLogout}>
+        Logout
+      </button>
+    )}
   </header>
 );
 
