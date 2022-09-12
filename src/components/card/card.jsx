a1c753a8564613614f275d9d6b4b1fb323012e5c
nneo <aisapple1030@naver.com>
2022-09-12 14:07:22 +0900

    [FEAT] add hover effect and transition

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,38 +5,40 @@ import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ card }) => {
   return (
-    <section className={`${styles.card} ${getColors(card.theme)}`}>
-      <div className={styles.header}>
-        <div className={styles.info}>
-          <div className={`${styles.date} ${getColors(card.theme)}`}>
-            {card.date}
+    <div className={styles.cardPack}>
+      <section className={`${styles.card} ${getColors(card.theme)}`}>
+        <div className={styles.header}>
+          <div className={styles.info}>
+            <div className={`${styles.date} ${getColors(card.theme)}`}>
+              {card.date}
+            </div>
+            <button className={`${styles.more} ${getColors(card.theme)}`}>
+              <FontAwesomeIcon icon={faEllipsis} />
+            </button>
           </div>
-          <button className={`${styles.more} ${getColors(card.theme)}`}>
-            <FontAwesomeIcon icon={faEllipsis} />
+          <div className={styles.tags}>
+            {card.tags.map((tag, index) => {
+              return (
+                <div
+                  key={index}
+                  className={` ${styles.tag} ${getColors(card.theme)}`}
+                >
+                  {tag}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <button className={`${styles.count} ${getColors(card.theme)}`}>
+            ❤{card.count}
           </button>
+          <h4 className={`${styles.title} ${getColors(card.theme)}`}>
+            {card.title}
+          </h4>
         </div>
-        <div className={styles.tags}>
-          {card.tags.map((tag, index) => {
-            return (
-              <div
-                key={index}
-                className={` ${styles.tag} ${getColors(card.theme)}`}
-              >
-                {tag}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <button className={`${styles.count} ${getColors(card.theme)}`}>
-          ❤{card.count}
-        </button>
-        <h4 className={`${styles.title} ${getColors(card.theme)}`}>
-          {card.title}
-        </h4>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
