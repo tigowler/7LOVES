00b36a63f2f4840a5cf7d44c1ce2746362718643
nneo <aisapple1030@naver.com>
2022-09-12 16:10:27 +0900

    [FEAT] footer css

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,17 +2,34 @@ import styles from "./footer.module.css";
 import React from "react";
 
 const Footer = ({ instruction, selectedTitle }) => {
+  const onEditClick = () => {
+    console.log("Edit!");
+  };
+  const onDeleteClick = () => {
+    console.log("Delete!");
+  };
+  const onCancelClick = () => {
+    console.log("Cancel!");
+  };
   return (
-    <footer>
+    <footer className={styles.footer}>
       {instruction === "options" && (
-        <div>
-          <h4>You selected</h4>
-          <h4>{selectedTitle}.</h4>
-          <h4>You wanna</h4>
-          <button>Edit</button>
-          <button>Delete</button>
-          <button>Cancel</button>
-          <h4>?</h4>
+        <div className={styles.options}>
+          <h4 className={styles.message}>You selected</h4>
+          <h4 className={styles.title}>{selectedTitle}</h4>
+          <h4 className={styles.message}>You wanna</h4>
+          <div className={styles.buttons}>
+            <button className={styles.button} onClick={onEditClick}>
+              Edit
+            </button>
+            <button className={styles.button} onClick={onDeleteClick}>
+              Delete
+            </button>
+            <button className={styles.button} onClick={onCancelClick}>
+              Cancel
+            </button>
+          </div>
+          <h4 className={styles.message}>?</h4>
         </div>
       )}
     </footer>
